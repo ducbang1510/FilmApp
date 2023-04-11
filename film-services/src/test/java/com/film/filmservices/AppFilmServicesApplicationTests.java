55216290105bef8999a7e43411a500b039b89af6
Trần Đức Bằng <bangtran146@gmail.com>
2023-04-11 14:44:18 +0700

    Init Project

---
+++ film-services/src/test/java/com/film/filmservices/AppFilmServicesApplicationTests.java
@@ -0,0 +1,13 @@
+package com.film.filmservices;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class AppFilmServicesApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
---
+++ user-services/src/test/java/com/user/userservices/UserPortalServicesApplicationTests.java
@@ -0,0 +1,13 @@
+package com.user.userservices;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class UserPortalServicesApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
